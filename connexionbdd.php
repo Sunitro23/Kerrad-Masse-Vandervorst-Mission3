635358889a5741ccc19a566aafdf0c3e71a7e48a
Sunitro23 <98162703+Sunitro23@users.noreply.github.com>
2022-03-21 16:57:44 +0100

    Delete connexionbdd.php

--- connexionbdd.php
+++
@@ -1,9 +0,0 @@
-<?php
-function connexion(){
-try {
-    $connect = new PDO('mysql:host=localhost;dbname=krd;charset=utf8', 'root', '');
-} catch (Exception $ex) {
-    echo 'Connexion �chou�e : ' . $e->getMessage();
-}
-return $connect;
-}
