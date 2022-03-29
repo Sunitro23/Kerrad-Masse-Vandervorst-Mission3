5204ff89e73cdb525ac90c2d9e7c1f69549f342d
Sunitro23 <98162703+Sunitro23@users.noreply.github.com>
2022-03-29 11:35:22 +0200

    Fonction Tri CSS Update

--- tests/testgetTRi.php
+++ tests/testgetTRi.php
@@ -1,25 +1,35 @@
 <?php
-
 include '../connexionbdd.php';
 include '../fonctionsBDD.php';
-
 $pdo = connect();
+?>
 
-echo '<form action="#" method = "POST">';
-
-getSelectType($pdo);
-
-getSelectVille($pdo);
-
-echo '<input type="submit"></form>';
-
+<!DOCTYPE html>
+<link href="../newcss.css" rel="stylesheet">
+<form action="#" method = "POST">
+    <?php
+    $LesTypes = getSelectType($pdo);
+    $LesVilles = getSelectVille($pdo);
+    ?>
+    <input type="submit"></form>
+<?php
 if (isset($_POST['typeSelect'])) {
-    $type = $_POST['typeSelect'];
-    $ville = $_POST['villeSelect'];
+    $type = $_POST['Type'];
+    $ville = $_POST['Ville'];
     $lesBiens = getTri($pdo, $type, $ville);
     foreach ($lesBiens as $unBien) {
-        echo'<p> Prix :' . $unBien['prix'] . ' euros /  ';
-        echo'Ville :' . $unBien['ville'] . ' / ';
-        echo'Type :' . $unBien['idtype'] . '</p>';
+        echo '<div class="cadre">
+            <img src="..' . $unBien['chemin'] . '" alt="' . $unBien['libelle'] . '" width="300" height="300">
+            </div>
+             <div class="prop-bien">
+                <p>' . $unBien['prix'] . '€</p>
+                <p>' . $unBien['idtype'] . '</p>
+                <p>' . $unBien['ville'] . ' ' . $unBien['rue'] . '</p>
+                <p>' . $unBien['nbpieces'] . ' pièces</p>
+                <p>' . $unBien['superficie'] . ' mètres carrés</p>
+             </div>
+        </div>
+        </body>';
     }
 }
+?>
