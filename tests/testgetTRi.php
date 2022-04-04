5847d836ca1655adbef6c82c535078c819e1f5e8
LaelV <91066171+LaelV@users.noreply.github.com>
2022-04-04 17:10:54 +0200

    changement tri

--- tests/testgetTRi.php
+++ tests/testgetTRi.php
@@ -1,35 +1,34 @@
 <?php
+
 include '../connexionbdd.php';
 include '../fonctionsBDD.php';
 $pdo = connect();
-?>
+echo'<!DOCTYPE html><link href="../newcss.css" rel="stylesheet"><form action="#" method = "POST">';
+$LesTypes = getSelectType($pdo);
+$LesVilles = getSelectVille($pdo);
+echo '<label for="jardin">Avec jardin</label><input type="checkbox" id="jardin" name="jardin" value="jardin"><br><input type="submit"></form>';
 
-<!DOCTYPE html>
-<link href="../newcss.css" rel="stylesheet">
-<form action="#" method = "POST">
-    <?php
-    $LesTypes = getSelectType($pdo);
-    $LesVilles = getSelectVille($pdo);
-    ?>
-    <input type="submit"></form>
-<?php
 if (isset($_POST['typeSelect'])) {
     $type = $_POST['typeSelect'];
     $ville = $_POST['villeSelect'];
-    $lesBiens = getTri($pdo, $type, $ville);
+    if ($_POST['jardin']=="jardin"){
+        $jardin=true;
+    }
+    else{
+        $jardin=false;
+    }
+    $lesBiens = getTri($pdo, $type, $ville,$jardin);
     foreach ($lesBiens as $unBien) {
         echo '<div class="cadre">
             <img src="..' . $unBien['chemin'] . '" alt="' . $unBien['libelle'] . '" width="300" height="300">
+                <div class="prop-bien">
+                    <p>' . $unBien['prix'] . '€</p>
+                    <p>' . $unBien['idtype'] . '</p>
+                    <p>' . $unBien['ville'] . ' ' . $unBien['rue'] . '</p>
+                    <p>' . $unBien['nbpieces'] . ' pièces</p>
+                    <p>' . $unBien['superficie'] . ' mètres carrés</p>
+                </div>
             </div>
-             <div class="prop-bien">
-                <p>' . $unBien['prix'] . '€</p>
-                <p>' . $unBien['idtype'] . '</p>
-                <p>' . $unBien['ville'] . ' ' . $unBien['rue'] . '</p>
-                <p>' . $unBien['nbpieces'] . ' pièces</p>
-                <p>' . $unBien['superficie'] . ' mètres carrés</p>
-             </div>
-        </div>
         </body>';
     }
 }
-?>
