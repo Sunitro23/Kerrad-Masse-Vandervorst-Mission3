3fba28f0161aeeecd12512a8804a501d98a6f6e9
Sunitro23 <98162703+Sunitro23@users.noreply.github.com>
2022-04-04 16:27:27 +0200

    Tri Update

--- fonctionsBDD.php
+++ fonctionsBDD.php
@@ -1,38 +1,24 @@
 <?php
 
-
-function getTri($pdo, $type, $ville) {
-    //Verif si type/ville doit être trié ou pas
-    if ($type != 0 && $ville != strval(0)) {
-        
-        $query = $pdo->prepare('SELECT * FROM image JOIN bien ON bien.id = image.idbien WHERE idtype = :type AND ville= :ville');
-        $bind1 = $query->bindValue(':type', $type, PDO::PARAM_STR);
-        $bind2 = $query->bindValue(':ville', $ville, PDO::PARAM_STR);
-        
-    } elseif ($type != 0) {
-        
-        $query = $pdo->prepare('SELECT * FROM image JOIN bien ON bien.id = image.idbien WHERE idtype = :type');
-        $bind1 = $query->bindValue(':type', $type, PDO::PARAM_STR);
-        
-    } elseif ($ville !=  strval(0)) {
-        
-        $query = $pdo->prepare('SELECT * FROM image JOIN bien ON bien.id = image.idbien WHERE ville= :ville');
-        $bind2 = $query->bindValue(':ville', $ville, PDO::PARAM_STR);
-        
-    } else {
-        
-        $query = $pdo->prepare('SELECT * FROM image JOIN bien ON bien.id = image.idbien');
-    }
+function getTri($connect, $type, $ville) {
+    $requete = "SELECT * FROM bien INNER JOIN image ON bien.id = image.idbien WHERE 1=1 ";
     
-    //Execution
-    $query->execute();
+    if ($type != 0) {$requete .= " AND idtype = :idtype";}
+    if ($ville != "0"){$requete .= " AND ville = :ville";}
+    
+    $query = $connect->prepare($requete);
+    
+    if ($type != 0) {$bv1 = $query->bindValue(':idtype', $type, PDO::PARAM_STR);}
+    if ($ville != "0") {$bv2 = $query->bindValue(':ville', $ville, PDO::PARAM_STR);}
+    
+    $execute = $query->execute();
     $LesBiens = $query->fetchAll();
     return $LesBiens;
 }
 
-function getSelectType($pdo) {
+function getSelectType($connect) {
     //Requete
-    $query = $pdo->prepare('SELECT * FROM type');
+    $query = $connect->prepare('SELECT * FROM type');
     $execute = $query->execute();
     $LesTypes = $query->fetchAll();
     //Affichage Select HTML
@@ -43,9 +29,9 @@ function getSelectType($pdo) {
     echo '</select>';
 }
 
-function getSelectVille($pdo) {
+function getSelectVille($connect) {
     //Requete
-    $query = $pdo->prepare('SELECT DISTINCT ville FROM bien');
+    $query = $connect->prepare('SELECT DISTINCT ville FROM bien');
     $execute = $query->execute();
     $LesVilles = $query->fetchAll();
     //Affichage Select HTML
@@ -55,4 +41,3 @@ function getSelectVille($pdo) {
     }
     echo '</select>';
 }
-
